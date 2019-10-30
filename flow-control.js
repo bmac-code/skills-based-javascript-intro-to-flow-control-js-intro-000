dbdc6906499ff0258b1f47ec5cec92e11c96d665
B Mac <bjgamc@gmail.com>
2019-10-30 18:52:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -33,7 +33,7 @@ return (age >=13 && age <=19) ? "You are a teenager":"You are not a teenager"
 function switchAge(age) {
 switch (age) {
   case 13 || 14 || 15 || 16 || 17 || 18 || 19:
-    return "You area teenager"
+    return "You are teenager"
 
   default:
   return "You have an age"
